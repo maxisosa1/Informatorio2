ebcea60379e3e40832cbef9a00fb54c6d72fee1d
Maximiliano Sosa <maximora470@gmail.com>
2023-09-22 19:38:13 -0300

    Prueba dia 2

---
+++ variables1_inf.py
@@ -0,0 +1,29 @@
+
+dicc = {"fruta1" : "manzana", "fruta2" : "pera", "fruta3" : "frutilla"}
+print(dicc)
+
+dic2 = {"ciudad1" : "Paris", "ciudad2" : "Nueva York", "ciudad3" : "Buenos Aires"} 
+dic2["ciudad4"] = "Resistencia"
+print(dic2)
+
+print(dic2["ciudad2"])
+
+set1 = {1,2,3,4,5,6,7,8,9,10}
+print(max(set1))
+
+set1 = (1,3,5,7,9)
+print(len(set1))
+
+lista1  = [1,2,3,4,5,6,7,8,9,10]
+
+print(lista1[: -1 ])
+
+lista2 = ["pera", "manzana", "aguacate"]
+lista2.remove("manzana")
+print(lista2)
+
+tupla = (1,2,3,4,5)
+suma = 0
+for n in tupla:
+    suma += n
+print(suma)
