fef0ad3f55df1e8b77b49c7c6c0f26ad80953d90
Maximiliano Sosa <maximora470@gmail.com>
2023-10-13 16:12:50 -0300

    Cambios 13/10

--- informatorio_semana3y4.py
+++ informatorio_semana3y4.py
@@ -1,21 +1,44 @@
-palabra = input("Dame una palabra: ")
-palabra.lower()
-palabra = list(palabra)
+def cadena_invertida(cadena):
+    return cadena[::-1]
 
-if "a" not in palabra and "e" not in palabra and "i" not in palabra and "o" not in palabra and "u" not in palabra:
-    print("Tu palabra no tiene vocales")
-else:
-    for letra in palabra:
-        indice = palabra.index(letra)
-        if letra == "a":
-            palabra[indice] = "A"
-        elif letra == "e":
-            palabra[indice] = "E"
-        elif letra == "i":
-            palabra[indice] = "I"
-        elif letra == "o":
-            palabra[indice] = "O"
-        elif letra == "u":
-            palabra[indice] = "U"
 
-print("".join(palabra))
+print(cadena_invertida("Hola"))
+
+
+def es_divisible(a, b):
+    if a % b == 0:
+        print(f"{a} es divisible por {b}")
+    else:
+        print(f"{a} NO es divisible por {b}")
+
+
+es_divisible(7, 10)
+
+
+def imprimir_pares(n):
+    lista = []
+    for num in range(n):
+        if num % 2 == 0:
+            lista.append(num)
+        else:
+            continue
+    return lista
+
+
+print(imprimir_pares(100))
+
+
+def promedio(*args):
+    return sum(args) / len(args)
+
+
+print(promedio(2, 4, 8, 9, 10))
+
+def factorial(numero):
+    total = numero
+    for n in range(1, numero):
+        total *= n
+    return total
+
+print(factorial(5))
+
